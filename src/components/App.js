4141eb0b28a2fa7da59a62e324a035467543722c
Saki <sbasken@gmail.com>
2023-03-01 18:02:51 -0800

    complete all deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,35 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  const adoptPet = (id) => {
+    const updatedPets = pets.map(pet => {
+      if (pet.id === id) {
+        return {...pet, isAdopted: true}
+      } else {
+        return pet
+      }
+    })
+    setPets(updatedPets)
+  }
+
+  const findPetsClicked = () => {
+    if (filters.type === "all") {
+      fetch("http://localhost:3001/pets")
+        .then(res => res.json())
+        .then(data => setPets(data))
+    } else {
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+        .then(res => res.json())
+        .then(data => setPets(data))
+    }
+  }
+
+  const handleChangeType = (option) => {
+    setFilters({ type: option })
+    console.log(filters)
+  }
+
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +44,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleChangeType} onFindPetsClick={findPetsClicked}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={adoptPet}/>
           </div>
         </div>
       </div>
